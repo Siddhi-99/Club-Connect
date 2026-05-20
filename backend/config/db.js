8f8470c220431dfbb796dc433433dd9d10f6ab4c
Siddhi-99 <siddhi.patil2512@gmail.com>
2026-05-20 16:49:10 +0530

    docker implemented

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Club_connect");
+    await mongoose.connect("mongodb://mongodb:27017/Club_connect");
     console.log("MongoDB Connected Successfully!");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
